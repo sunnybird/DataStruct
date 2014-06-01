f01ab246210a1da207990e1921c61b0270b62d7b
feng <jinlongfeng123@gmail.com>
2014-06-01 12:51:35 +0800

    add game000.cap

---
+++ game000.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<cstring>
+
+using namespace std;
+ifstream fin("data.in");
+ofstream fout("data.out");
+int main(){
+int line = 0 ;
+char buffer[100];
+while(fin.getline(buffer,100)){
+cout<<line++<<":"<<buffer<<endl;
+fout<<buffer<<endl;
+}
+return 0;
+}
+
